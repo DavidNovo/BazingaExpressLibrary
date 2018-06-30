08cd7db8ff8182aba26ea06e8ac1a8fd68150c2f
Unknown <david.novogrodsky@gmail.com>
2018-06-29 21:58:01 -0500

    Completed the Update Book form
    Wow, the update book form is done.  This is great.  It was tricky, lots of () and {} to balance.

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,10 @@ var express = require('express')
 var router = express.Router()
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
-  res.render('home', { title: 'Bazinga Express Library' })
+router.get('/', function(req, res, next) {
+  res.render('home', {
+    title: 'Bazinga Express Library'
+  })
 })
 
 module.exports = router
